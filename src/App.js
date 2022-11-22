6946f850f9cb0cb484372dab58948ea5a58bd2c5
Rohit Kumar <rohit.rew@gmail.com>
2022-11-22 17:02:48 +0530

    key prop added and favicon added

--- src/App.js
+++ src/App.js
@@ -1,36 +1,29 @@
-import './App.css';
-import React from 'react'
-import Person from './person';
-import Heading from './heading';
-import data from './data';
+import "./App.css";
+import React from "react";
+import Person from "./person";
+import Heading from "./heading";
+import data from "./data";
 
-export default function App(){
+export default function App() {
+  let [persons, changepersons] = React.useState(data);
 
+  let jsxperson = persons.map(function (person, i) {
+    return (
+      <Person key={i} name={person.Name} age={person.Age} img={person.img} />
+    );
+  });
 
-let [persons , changepersons] = React.useState(data)
-
-
-    let jsxperson = persons.map(function(person , i){
-      return <Person key={i} name={person.Name} age={person.Age} img={person.img} /> 
-    })
-
-    function clear(){
-      changepersons([])
-    }
-
+  function clear() {
+    changepersons([]);
+  }
 
   return (
     <main>
       <div className="container">
-              <Heading birthdays={persons.length} />
-              {jsxperson}
-             
-               
-            
-            <button onClick={clear}>Clear all</button>
+        <Heading birthdays={persons.length} />
+        {jsxperson}
+        <button onClick={clear}>Clear all</button>
       </div>
     </main>
-  )
-
-
+  );
 }
--- src/person.js
+++ src/person.js
@@ -1,11 +1,8 @@
-import react from 'react'
-// import image from "../src/image.png"
-
 
 export default function Person(props){
     return (
         <div className="personcontainer">
-        <img src={require(`../src/images/${props.img}`)} className='proimage' />
+        <img src={require(`../src/images/${props.img}`)} className='proimage' alt=""/>
               <div>
                 <p className="name">{props.name}</p>
                 <p className="age">{props.age} Years</p>
